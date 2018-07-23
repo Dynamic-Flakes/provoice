c9d5df8574f471d7264ce2583b4940fb1b00c8db
Engr.Sunday <asmelitus@gmail.com>
2018-07-23 22:55:09 +0100

    mongodb connection refinment

--- packages/provoice-api/src/config/persistence/mongodb/index.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-import connect from './connectdb';
-
-export const close = mongo => mongo.disconnect();
-
-export default async config => await connect(mongoose, config);
--- packages/provoice-api/src/server/index.js
+++ packages/provoice-api/src/server/index.js
@@ -3,9 +3,9 @@ import app from '../config/express';
 import mongoose from '../config/persistence/mongodb/connectdb';
 
 // open mongoose connection
-mongoose.connect();
+require('../config/persistence/mongodb/connectdb');
 
-
+// listern to the port
 const port = config.get('server.port');
 app.listen(port, () => console.info(`server started on port ${port}`));
 

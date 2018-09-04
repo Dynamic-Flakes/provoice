6ee10a97db3828ca1a722b298ec29a2c244eff84
Engr.Sunday <asmelitus@gmail.com>
2018-09-04 21:55:18 +0100

    :fire: separate middlewares from express config files

--- packages/provoice-api/src/config/express.js
+++ packages/provoice-api/src/config/express.js
@@ -1,48 +1,18 @@
 const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const compress = require('compression');
-const methodOverride = require('method-override');
-const cors = require('cors');
-const helmet = require('helmet');
-const passport = require('passport');
 
 const routes = require('../routes/v1');
-const { logs } = require('./vars');
-const strategies = require('./passport');
 const error = require('../middlewares/error');
-
+const middlewaresConfig = require('./middlewares')
 /**
 * Express instance
 * @public
 */
 const app = express();
 
-// request logging. dev: console | production: file
-app.use(morgan(logs));
-
-// parse body params and attache them to req.body
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// gzip compression
-app.use(compress());
-
-// lets you use HTTP verbs such as PUT or DELETE
-// in places where the client doesn't support it
-app.use(methodOverride());
-
-// secure apps by setting various HTTP headers
-app.use(helmet());
-
-// enable CORS - Cross Origin Resource Sharing
-app.use(cors());
-
-// enable authentication
-app.use(passport.initialize());
-passport.use('jwt', strategies.jwt);
-passport.use('facebook', strategies.facebook);
-passport.use('google', strategies.google);
+/**
+* Middlewares
+*/
+middlewaresConfig(app);
 
 // mount api v1 routes
 app.use('/v1', routes);
---
+++ packages/provoice-api/src/config/middlewares.js
@@ -0,0 +1,25 @@
+const bodyParser = required('body-parser');
+const morgan = required('morgan');
+const passport = require('passport');
+const compress = require('compression');
+const methodOverride = require('method-override');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const { logs } = require('./vars');
+const strategies = require('./passport');
+
+module.exports = app => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(morgan(logs));
+  app.use(compress());
+  app.use(methodOverride());
+  app.use(helmet());
+  app.use(cors());
+  app.use(passport.initialize());
+  passport.use('jwt', strategies.jwt);
+  passport.use('facebook', strategies.facebook);
+  passport.use('google', strategies.google);
+};
+
